22ee2e358251db255f753a76843bf511218baa53
Toby Fee <TobyFee@gmail.com>
2018-07-31 12:29:57 -0700

    debugging

--- Stackery/functions/List Accounts/index.js
+++ Stackery/functions/List Accounts/index.js
@@ -3,12 +3,12 @@ const knex = require('knex');
 const connectionName = process.env.CONNECTION || 'development';
 const connection = require('knexfile')[connectionName];
 
-var iopipe = require('@iopipe/iopipe')({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlNTIzMmYwZi0yYmIxLTQzZTEtOTljZC1kMmJhNDc2ZWJmOTEiLCJqdGkiOiJjZGVmMmQ3NC1iOGYzLTQzYzgtOTBkNS0yMWNlNzg2NTFhM2QiLCJpYXQiOjE1MzMwNjEzOTMsImlzcyI6Imh0dHBzOi8vaW9waXBlLmNvbSIsImF1ZCI6Imh0dHBzOi8vaW9waXBlLmNvbSxodHRwczovL21ldHJpY3MtYXBpLmlvcGlwZS5jb20vZXZlbnQvLGh0dHBzOi8vZ3JhcGhxbC5pb3BpcGUuY29tIn0.UvWdEuZA8PI2eZmIiJ9YzpkUtzooCR0v1P9iTp61Gas" });
+//var iopipe = require('@iopipe/iopipe')({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlNTIzMmYwZi0yYmIxLTQzZTEtOTljZC1kMmJhNDc2ZWJmOTEiLCJqdGkiOiJjZGVmMmQ3NC1iOGYzLTQzYzgtOTBkNS0yMWNlNzg2NTFhM2QiLCJpYXQiOjE1MzMwNjEzOTMsImlzcyI6Imh0dHBzOi8vaW9waXBlLmNvbSIsImF1ZCI6Imh0dHBzOi8vaW9waXBlLmNvbSxodHRwczovL21ldHJpY3MtYXBpLmlvcGlwZS5jb20vZXZlbnQvLGh0dHBzOi8vZ3JhcGhxbC5pb3BpcGUuY29tIn0.UvWdEuZA8PI2eZmIiJ9YzpkUtzooCR0v1P9iTp61Gas" });
 /**
  * Fetch list of accounts from database and respond with an array of account
  * names.
  */
-module.exports = iopipe(async message => {
+module.exports = async message => {
   const client = knex(connection);
 
   try {
@@ -18,4 +18,4 @@ module.exports = iopipe(async message => {
   } finally {
     client.destroy();
   }
-});
+};
